0af2b2506f8d61958d3eaae5c6748ec92ed16962
nedik <kknedi@gmail.com>
2024-11-03 20:25:24 +0000

    [Map editor] ColorPickerTool can get color under cursor from polygon vertices and sceneries

--- source/client/map_editor/tools/ColorPickerTool.cpp
+++ source/client/map_editor/tools/ColorPickerTool.cpp
@@ -1,5 +1,7 @@
 #include "map_editor/tools/ColorPickerTool.hpp"
 
+#include "core/math/Calc.hpp"
+
 namespace Soldank
 {
 void ColorPickerTool::OnSelect(ClientState& client_state, const State& game_state) {}
@@ -14,6 +16,45 @@ void ColorPickerTool::OnSceneLeftMouseButtonClick(ClientState& client_state,
 void ColorPickerTool::OnSceneLeftMouseButtonRelease(ClientState& client_state,
                                                     const State& game_state)
 {
+    float max_square_distance = 40.0F * 40.0F;
+
+    for (int i = game_state.map.GetPolygonsCount() - 1; i >= 0; --i) {
+        const auto& polygon = game_state.map.GetPolygons().at(i);
+
+        if (Map::PointInPoly(client_state.mouse_map_position, polygon)) {
+            for (const auto& vertex : polygon.vertices) {
+                if (Calc::SquareDistance(client_state.mouse_map_position, { vertex.x, vertex.y }) <=
+                    max_square_distance) {
+
+                    client_state.map_editor_state.palette_current_color.at(0) =
+                      (float)vertex.color.red / 255.0F;
+                    client_state.map_editor_state.palette_current_color.at(1) =
+                      (float)vertex.color.green / 255.0F;
+                    client_state.map_editor_state.palette_current_color.at(2) =
+                      (float)vertex.color.blue / 255.0F;
+                    client_state.map_editor_state.palette_current_color.at(3) =
+                      (float)vertex.color.alpha / 255.0F;
+                    return;
+                }
+            }
+        }
+    }
+
+    for (int i = game_state.map.GetSceneryInstances().size() - 1; i >= 0; --i) {
+        const auto& scenery = game_state.map.GetSceneryInstances().at(i);
+
+        if (Map::PointInScenery(client_state.mouse_map_position, scenery)) {
+            client_state.map_editor_state.palette_current_color.at(0) =
+              (float)scenery.color.red / 255.0F;
+            client_state.map_editor_state.palette_current_color.at(1) =
+              (float)scenery.color.green / 255.0F;
+            client_state.map_editor_state.palette_current_color.at(2) =
+              (float)scenery.color.blue / 255.0F;
+            client_state.map_editor_state.palette_current_color.at(3) =
+              (float)scenery.color.alpha / 255.0F;
+            return;
+        }
+    }
 }
 
 void ColorPickerTool::OnSceneRightMouseButtonClick(ClientState& client_state) {}
