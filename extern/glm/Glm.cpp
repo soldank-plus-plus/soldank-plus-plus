8c6b111103b41e2b52136808b062cc9180ef7637
nedik <kknedi@gmail.com>
2026-07-23 01:28:17 +0200

    fix(build): add missing glm parts in Glm wrapper

--- extern/glm/Glm.cpp
+++ extern/glm/Glm.cpp
@@ -11,23 +11,24 @@ export using namespace glm;
 
 export namespace glm
 {
+using ::glm::distance;
+using ::glm::dot;
 using ::glm::dvec2;
 using ::glm::ivec2;
-using ::glm::mat4;
-using ::glm::uvec2;
-using ::glm::vec2;
-using ::glm::vec3;
-using ::glm::vec4;
-using ::glm::dot;
 using ::glm::length;
+using ::glm::mat4;
 using ::glm::mix;
 using ::glm::ortho;
 using ::glm::rotate;
 using ::glm::scale;
 using ::glm::translate;
+using ::glm::uvec2;
 using ::glm::value_ptr;
+using ::glm::vec2;
+using ::glm::vec3;
+using ::glm::vec4;
 using ::glm::operator+;
 using ::glm::operator-;
 using ::glm::operator*;
 using ::glm::operator/;
-}
+} // namespace glm
